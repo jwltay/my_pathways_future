60454b6995febacfbcc3c1bc0a94dd12b403d5cf
deepika joshi <upreti.deepika@gmail.com>
2023-03-20 17:43:24 +0800

    added soft_skill model

---
+++ app/models/soft_skill.rb
@@ -0,0 +1,2 @@
+class SoftSkill < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_20_093810) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_20_094210) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_093810) do
     t.integer "duration"
     t.integer "rating"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "soft_skills", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
