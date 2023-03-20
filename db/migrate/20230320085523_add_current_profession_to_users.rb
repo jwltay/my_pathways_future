7079ba47fcd6cc3653e565996b1f06a1c4fa5b85
deepika joshi <upreti.deepika@gmail.com>
2023-03-20 17:01:42 +0800

    removed null :false from current profession

--- db/migrate/20230320085523_add_current_profession_to_users.rb
+++ db/migrate/20230320085523_add_current_profession_to_users.rb
@@ -1,5 +1,5 @@
 class AddCurrentProfessionToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :current_profession, :string, null: false
+    add_column :users, :current_profession, :string
   end
 end
