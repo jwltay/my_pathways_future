997dfe3f7fde5e480564d49855feca2d93322aac
deepika joshi <upreti.deepika@gmail.com>
2023-03-20 17:00:20 +0800

    added null : false to current profession and age

--- db/migrate/20230320085523_add_current_profession_to_users.rb
+++ db/migrate/20230320085523_add_current_profession_to_users.rb
@@ -1,5 +1,5 @@
 class AddCurrentProfessionToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :current_profession, :string
+    add_column :users, :current_profession, :string, null: false
   end
 end
--- db/migrate/20230320085722_add_age_to_users.rb
+++ db/migrate/20230320085722_add_age_to_users.rb
@@ -1,5 +1,5 @@
 class AddAgeToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :age, :integer
+    add_column :users, :age, :integer, null: false
   end
 end
