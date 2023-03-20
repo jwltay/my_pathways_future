e4f56f5b0d6c07c1834f2edeb4df81f9c33f99ab
deepika joshi <upreti.deepika@gmail.com>
2023-03-20 17:51:13 +0800

    added pathway model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_20_094341) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_20_094825) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_094341) do
     t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "pathways", force: :cascade do |t|
+    t.boolean "active", default: false, null: false
+    t.boolean "primary", default: false, null: false
+    t.bigint "user_id", null: false
+    t.bigint "programme_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["programme_id"], name: "index_pathways_on_programme_id"
+    t.index ["user_id"], name: "index_pathways_on_user_id"
   end
 
   create_table "programmes", force: :cascade do |t|
@@ -58,4 +69,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_094341) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pathways", "programmes"
+  add_foreign_key "pathways", "users"
 end
