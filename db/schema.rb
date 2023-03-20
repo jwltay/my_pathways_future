24c30e64ac1eb121a7dedf0a0c5824cb58344fc3
deepika joshi <upreti.deepika@gmail.com>
2023-03-20 17:41:10 +0800

    added career model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_20_093226) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_20_093810) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "careers", force: :cascade do |t|
+    t.integer "average_salary", null: false
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "programmes", force: :cascade do |t|
     t.integer "tuition_fees"
