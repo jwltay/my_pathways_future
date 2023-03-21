8282a5859302236879fc12d7ebb5a18b5a7bcacf
deepika joshi <upreti.deepika@gmail.com>
2023-03-21 15:35:54 +0800

    created view file for user index

--- app/controllers/programmes_controller.rb
+++ app/controllers/programmes_controller.rb
@@ -7,36 +7,36 @@ class ProgrammesController < ApplicationController
     @programme = Programme.find(params[:id])
   end
 
-  def new
-    @programme = Programme.new
-  end
+  # def new
+  #   @programme = Programme.new
+  # end
 
-  def create
-    @programme = Programme.new(job_params)
-    @programme.user = current_user
-    if @programme.save
-      redirect_to programme_path(@programme)
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
+  # def create
+  #   @programme = Programme.new(job_params)
+  #   @programme.user = current_user
+  #   if @programme.save
+  #     redirect_to programme_path(@programme)
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
 
-  def edit
-    @programme = Programme.find(params[:id])
-  end
+  # def edit
+  #   @programme = Programme.find(params[:id])
+  # end
 
-  def update
-    @programme = Programme.find(params[:id])
-    @programme.update!(programme_params)
-    redirect to programme_path(current_user)
-  end
+  # def update
+  #   @programme = Programme.find(params[:id])
+  #   @programme.update!(programme_params)
+  #   redirect to programme_path(current_user)
+  # end
 
-  def delete
-  end
+  # def delete
+  # end
 
-  private
+  # private
 
-  def programme_params
-    params.require(:programme).permit(:tuition_fees, :duration, :rating, :description, :summary)
-  end
+  # def programme_params
+  #   params.require(:programme).permit(:tuition_fees, :duration, :rating, :description, :summary)
+  # end
 end
