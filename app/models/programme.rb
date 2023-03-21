58430b6e3110c60f30714fef73f7b7ff7bbd20dd
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-03-21 18:07:03 +0800

    Adds association for soft_skill to career and programme to career

--- app/models/programme.rb
+++ app/models/programme.rb
@@ -1,7 +1,4 @@
 class Programme < ApplicationRecord
-  has_and_belongs_to_many :careers
-  has_and_belongs_to_many :subjects
-
   has_many :pathways
   has_many :users, through: :pathways
 
--- app/models/soft_skill.rb
+++ app/models/soft_skill.rb
@@ -2,5 +2,8 @@ class SoftSkill < ApplicationRecord
   has_many :user_soft_skills
   has_many :users, through: :user_soft_skills
 
+  has_many :career_soft_skills
+  has_many :career, through: :career_soft_skills
+
   validates :name, presence: true, uniqueness: true
 end
