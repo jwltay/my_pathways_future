b0e7b295861fd880c80cf288b2aa552745ffcc8b
deepika joshi <upreti.deepika@gmail.com>
2023-03-21 12:59:32 +0800

    added methods in pathways  controller

--- app/controllers/pathways_controller.rb
+++ app/controllers/pathways_controller.rb
@@ -1,2 +1,46 @@
 class PathwaysController < ApplicationController
+  before_action :set_programme, except: :update
+  before_action :authenticate_user!
+
+  def index
+    @pathways = Pathway.all
+  end
+
+  def new
+    @pathways = Pathway.new
+  end
+
+  def create
+    @pathway = Pathway.new(pathway_params)
+    @pathway.career = @career
+    @pathway.user = current_user
+    current_user.update(user_params)
+    if @pathway.save
+      flash[:notice] = "Pathway is saved!"
+      redirect_to programme_path(@programme)
+    else
+      render "programme/show", status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @pathway = Pathway.find(params[:id])
+    @pathway.save!
+    @programme = Programme.find(params[:programme_id])
+    redirect_to programme_path(@programme)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :current_profession, :occupation)
+  end
+
+  def pathway_params
+    params.require(:pathway).permit(:programme_id)
+  end
+
+  def set_programme
+    @programme = Programme.find(params[:programme_id])
+  end
 end
