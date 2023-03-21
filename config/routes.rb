8205c228bdd053da58945f4cafe37ab47fa492ec
deepika joshi <upreti.deepika@gmail.com>
2023-03-21 11:08:47 +0800

    added resources :soft_skills only for index and show

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :subjects, only: %i[index show]
+  resources :soft_skills, only: %i[index show]
 end
