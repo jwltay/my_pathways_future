0833815f2028458feab49b2c17dc05b2ecdacc1c
Leanne Tay <duckingville@yahoo.com>
2023-03-22 11:46:30 +0800

    wrote temporary seeds to test app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# temporary user and careers for testing
+# manu = User.create!(first_name: "Manu", email: "manu@email.com", password: "password", occupation: "student")
+
+# nus = Programme.create!(name: "bsc cs", university: "nus", tuition_fees: 30000, summary: "summary", overview: "overview", duration: 4, rating: 5)
+# ntu = Programme.create!(name: "bsc cs", university: "ntu", tuition_fees: 30000, summary: "summary", overview: "overview", duration: 4, rating: 5)
+# math = Programme.create!(name: "bsc mathematics", university: "ntu", tuition_fees: 30000, summary: "summary", overview: "overview", duration: 4, rating: 5)
+
+
+# swe = Career.create!(name: "swe", average_salary: 5000, summary: "summary", responsibilities: "responsibilities", progression: "progression", education: "education")
+# da = Career.create!(name: "data analyst", average_salary: 5000, summary: "summary", responsibilities: "responsibilities", progression: "progression", education: "education")
+
+# math.careers << da
+# math.careers << swe
+# nus.careers << swe
+# ntu.careers << swe
+
+
+# Pathway.create!(user: manu, programme: nus, active: true)
+# Pathway.create!(user: manu, programme: ntu, active: true)
+# Pathway.create!(user: manu, programme: math, active: true)
