c88038e28080706b1f773fee6522a4d139f96610
Leanne Tay <duckingville@yahoo.com>
2023-03-22 13:38:23 +0800

    form now updates user with correct information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,31 @@ class UsersController < ApplicationController
   def edit
     @user = current_user
   end
-  # def update
-  #   @user = current_user
-  #   @user.update(user_params)
-  #   redirect_to edit_user_path(current_user)
-  # end
 
-  # private
+  def update
+    @user = current_user
+    @user.first_name = user_params[:first_name]
+    @user.last_name = user_params[:last_name]
+    @user.occupation = user_params[:occupation]
+    user_params[:subject_ids].compact_blank.each { |id| @user.subjects << Subject.find(id) }
+    user_params[:soft_skill_ids].compact_blank.each { |id| @user.soft_skills << SoftSkill.find(id) }
+    if @user.save!
+      redirect_to edit_user_path(current_user)
+      # create pathways
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
-  # def user_params
-  #   params.require(:user).permit(:first_name, :last_name, :email, :current_profession, :occupation)
-  # end
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :occupation,
+      subject_ids: [],
+      soft_skill_ids: []
+    )
+  end
 end
