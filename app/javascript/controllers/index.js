1407bf0f90db14ac7f65b64f51e29cc9b44eb307
Leanne Tay <duckingville@yahoo.com>
2023-03-23 14:26:10 +0800

    form is split into different sections and displayed one by one using stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import UserFormController from "./user_form_controller"
+application.register("user-form", UserFormController)
