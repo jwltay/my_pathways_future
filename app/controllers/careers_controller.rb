b65beffb03c7b5f248075dd68b7ac9250bfdf8f1
Leanne Tay <duckingville@yahoo.com>
2023-03-26 13:43:46 +0800

    split saving of careers into json into multiple private methods

--- app/controllers/careers_controller.rb
+++ app/controllers/careers_controller.rb
@@ -4,39 +4,44 @@ class CareersController < ApplicationController
   def index
     @user = current_user
     @careers = @user.careers
-    nodes = [
-      {
+    generate_graph_json
+  end
+
+  def show
+    @career = Career.find(params[:id])
+  end
+
+  private
+
+  def graph_nodes
+    [{
         "id": @user.first_name.capitalize,
         "name": "id1",
         "val": "1"
-      }
-    ] + @careers.each_with_index.map do |career, index|
-      {
-        "id": career.name.titleize,
+    }] + @careers.each_with_index.map do |career, index|
+      { "id": career.name.titleize,
         "name": "id#{index + 2}",
-        "val": "1"
-      }
+        "val": "1" }
     end
+  end
 
-    links = @careers.map do |career|
+  def graph_links
+    @careers.map do |career|
       {
         "source": career.name.titleize,
         "target": @user.first_name.capitalize
       }
     end
+  end
 
-
+  def generate_graph_json
     graph_data = {
-      "nodes": nodes,
-      "links": links
+      "nodes": graph_nodes,
+      "links": graph_links
     }
 
     File.open('public/graph.json', "wb") do |file|
       file.write(JSON.generate(graph_data))
     end
-  end
-
-  def show
-    @career = Career.find(params[:id])
   end
 end
