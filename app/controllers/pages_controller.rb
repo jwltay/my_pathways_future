d172fa6654684df390ce37c7753675e455ea9a7a
deepika joshi <upreti.deepika@gmail.com>
2023-03-27 11:16:01 +0800

    added get dashboard in routes.rb file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    redirect_to :dashboard if current_user
+  end
+
+  def dashboard
   end
 end
