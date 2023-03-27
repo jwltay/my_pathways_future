072bfc3d2039840cad57f15fb6382a6e35780bdf
deepika joshi <upreti.deepika@gmail.com>
2023-03-27 15:35:20 +0800

    generated task controller and updates routes.rb only for create and update

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,10 @@
+class TasksController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+  get "tasks/index"
+  get "tasks/create"
+  get "tasks/update"
   devise_for :users, controllers: { registrations: "registrations" }
   root to: "pages#home"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
   get "/dashboard", to: "pages#dashboard"
+  resources :tasks, only: %i[create update]
   resources :programmes, only: %i[show] do
     resources :pathways, only: %i[create]
   end
