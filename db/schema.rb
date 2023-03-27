23e4e69e2bfd4b816003e3649390d44243dbc0df
deepika joshi <upreti.deepika@gmail.com>
2023-03-27 15:30:49 +0800

    migrated the task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_27_032929) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_27_072857) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -95,6 +95,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_032929) do
 
   create_table "subjects", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
