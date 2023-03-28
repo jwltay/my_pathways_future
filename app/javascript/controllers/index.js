796d3aa40537493d829ae643bb9d09d0c64b4ad1
deepika joshi <upreti.deepika@gmail.com>
2023-03-28 10:13:46 +0800

    added stimulus controller for todo list

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -19,5 +19,8 @@ application.register("modal", ModalController)
 import ShortlistController from "./shortlist_controller"
 application.register("shortlist", ShortlistController)
 
+import TodoListController from "./todo_list_controller"
+application.register("todo-list", TodoListController)
+
 import UserFormController from "./user_form_controller"
 application.register("user-form", UserFormController)
