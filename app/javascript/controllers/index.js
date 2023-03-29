8632dfda564dd9929c777ae48c4b1a27a3f33c4c
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-03-29 10:33:34 +0800

    Adds countdown stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,6 +10,9 @@ application.register("accordion", AccordionController)
 import CareersIndexController from "./careers_index_controller"
 application.register("careers-index", CareersIndexController)
 
+import CountdownController from "./countdown_controller"
+application.register("countdown", CountdownController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
