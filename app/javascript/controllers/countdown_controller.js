403a090d81d08229199ca84da39e69fa0d19a157
Zhi Qin Lee <leezhiqinuk@gmail.com>
2023-03-29 12:26:35 +0800

    Countdown timer functioning

--- app/javascript/controllers/countdown_controller.js
+++ app/javascript/controllers/countdown_controller.js
@@ -2,6 +2,37 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="countdown"
 export default class extends Controller {
+  static targets = ["countdown"]
+
   connect() {
+    console.log("Connected to countdown stimulus!")
+    this.secondsUntilEnd = this.countdownTarget.dataset.secondsUntilEnd;
+
+    const now = new Date().getTime();
+    this.endTime = new Date(now + this.secondsUntilEnd * 1000);
+
+    this.countdown = setInterval(this.countdown.bind(this), 1000);
+  }
+
+  countdown() {
+    const now = new Date();
+    const secondsRemaining = (this.endTime - now) / 1000;
+
+    if (secondsRemaining <= 0) {
+      clearInterval(this.countdown);
+      this.countdownTarget.innerHTML = "Countdown is up!";
+      return;
+    }
+
+    const secondsPerDay = 86400;
+    const secondsPerHour = 3600;
+    const secondsPerMinute = 60;
+
+    const days = Math.floor(secondsRemaining / secondsPerDay);
+    const hours = Math.floor((secondsRemaining % secondsPerDay) / secondsPerHour);
+    const minutes = Math.floor((secondsRemaining % secondsPerHour) / secondsPerMinute);
+    const seconds = Math.floor(secondsRemaining % secondsPerMinute);
+
+    this.countdownTarget.innerHTML = `${days} days : ${hours} hours : ${minutes} minutes : ${seconds} seconds`
   }
 }
